8f1b35547a131d45ef9dcee4403e74fce2aed2c5
RouaNafeh <rouanafeh6@gmail.com>
2026-03-23 16:44:38 +0200

    Base Android project with all activities

---
+++ app/src/main/java/com/example/dormchef/activities/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.dormchef.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dormchef.R;
+
+public class HomeActivity  extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
